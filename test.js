300ad308fb5fcc27c0dc4b27af313e28f9d1ef01
cicada1208 <plumes@cycu.org.tw>
2020-06-24 09:26:30 +0800

    test

--- test.js
+++ test.js
@@ -1,4 +1,29 @@
-console.log('local10');
+function main(require) {
+    var obj = require('./utils')
+    console.log(obj.cal(30))
+    console.log(obj.name)
+}
+
+function utils(module) {
+    function calculate(n) {
+        return ((n * 100 + 20 - 4)) % 10 + 3
+    }
+
+    module.exports = {
+        cal: calculate,
+        name: 'hello'
+    }
+}
+
+var m = {}
+utils(m)
+
+// 加入底下這幾行
+function r() {
+    // 回傳我們所需要的 m.exports
+    return m.exports
+}
+main(r)
 
 // import * as ex from './test_module.js';
 // console.log(ex.counter);
