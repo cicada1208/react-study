e31da13a9a381e9dd4e24bd4533a4d86132302a0
cicada1208 <plumes@cycu.org.tw>
2020-07-02 17:32:03 +0800

    React test

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,4 +1,9 @@
+// install webpack: npm install --save-dev webpack webpack-cli
+// bundle up moudle: npx webpack --config webpack.config.js
+
 module.exports = {
+    // mode: 'development' 開發模式
+    // mode: 'production' 產品模式，自動壓縮及優化
     mode: 'development',
     entry: './main.js',
     output: {
