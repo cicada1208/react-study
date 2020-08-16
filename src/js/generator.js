6a5f770466ca25e068edac3634f127e35b195275
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2020-08-16 16:12:44 +0800

    generator

--- src/js/generator.js
+++ src/js/generator.js
@@ -26,6 +26,10 @@ function* get_adder() {
     let total = 1;
     while (true) {
         console.log('test2:', "before yield");
+        // yield: 指令分成兩步驟，1.先丟東西出去，2.再等東西進來
+        // 因為此段是個迴圈
+        // 第一次呼叫 next()，執行至 yield 的步驟1.丟東西出去(yield 後面的表達式)
+        // 再次呼叫 next()，執行步驟2.等東西進來，再執行至 yield 的步驟1.丟東西出去
         total += yield total;
         console.log('test2:', "after yield, total:", total);
     }
@@ -39,3 +43,35 @@ console.log('test2:', adder.next(100).value);
 // test2: after yield, total: 101
 // test2: before yield
 // test2: 101
+
+
+// test3:
+function* test3(p) {
+    console.log('test3:', p); // test3: 1
+    var a = yield p + 1;
+    console.log('test3:', a); // test3: 3
+}
+
+var g = test3(1);
+var ret;
+ret = g.next();
+console.log('test3:', ret); // test3: { value: 2, done: false }
+ret = g.next(ret.value + 1);
+console.log('test3:', ret); // test3: { value: undefined, done: true }
+
+
+// test4:
+function* quips(name) {
+    yield "line1: " + name;
+    yield "line2";
+    if (name.startsWith("X")) {
+        yield "line3: " + name;
+    }
+    yield "line4";
+}
+
+var iter = quips('manman'); // [object Generator]
+console.log('test4:', iter.next()) // test4: { value: 'line1: manman', done: false }
+console.log('test4:', iter.next()) // test4: { value: 'line2', done: false }
+console.log('test4:', iter.next()) // test4: { value: 'line4', done: false }
+console.log('test4:', iter.next()) // test4: { value: undefined, done: true } // 抵達 generator function 尾端，done: true
