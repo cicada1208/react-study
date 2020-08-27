a37e7533d93b065b8832f9ec74872d69c99199d4
cicada1208 <plumes@cycu.org.tw>
2020-08-27 15:50:19 +0800

    react-router

--- src/js/react.ex.js
+++ src/js/react.ex.js
@@ -17,6 +17,7 @@ class Home extends React.Component {
                     <li><Link to="/todo">Todo</Link></li>
                     <li><Link to="/users/1">Users 1</Link></li>
                     <li><Link to="/users/2">Users 2</Link></li>
+                    <li><Link to="/render">render</Link></li>
                 </ul>
                 {this.props.children} {/* 對應的 component，例如：TodoTb */}
             </div>
@@ -40,19 +41,23 @@ divReactEx.id = 'divReactEx';
 document.body.appendChild(divReactEx);
 
 // <Route path="/" component={Home} />：
-// 此為非嚴格匹配，無論訪問什麼路徑，都會包含path="/"，故都會匹配到。
+// 此為非嚴格匹配，無論訪問什麼路徑，都會包含path="/"，故都會匹配到。
+
+// <Route component={Home} />：
+// 無指定 path，無論訪問什麼路徑，都會匹配到。結果同 <Route path="/" component={Home} />。
 
 // <Route exact path="/" component={Home} />：
 // 此為嚴格匹配，訪問根路徑 http://localhost:8008/ 才會匹配到。
 
-// <Route path="/todo" component={TodoTb} />：  
+// <Route path="/todo" component={TodoTb} />：
 // 瀏覽器地址輸入 http://localhost:8008/todo，React Router 匹配到，會在當前位置渲染對應的 component，
-// 相當於將 component TodoTb 內容替換掉 < Route path = "/todo" component = { TodoTb } /> 這行。
+// 相當於將 component TodoTb 內容替換掉 <Route path="/todo" component={TodoTb} /> 這行。
 ReactDOM.render(
     <HashRouter>
         <Route path="/" component={Home} />
         <Route path="/todo" component={TodoTb} />
         <Route path="/users/:userId" component={Users} />
+        <Route path="/render" render={() => { return <h1>我是匹配到的路由</h1> }} />
     </HashRouter>,
     divReactEx
 );
