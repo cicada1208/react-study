288e9ead086d1c8a5c9e4e3ff1be260878c0b6e4
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2020-09-04 00:01:59 +0800

    commomJS

--- src/js/webpack.cjs.utils.js
+++ src/js/webpack.cjs.utils.js
@@ -1,5 +1,9 @@
 // commomJS module:
 
+// 調用執行( require('module') ): 若該 module 透過 require 執行，require.main 指向 parent。
+console.log('require.main is module', require.main === module) // false
+console.log('require.main is module.parent', require.main === module.parent) // true
+
 function prnt() {
     return ' print'
 }
