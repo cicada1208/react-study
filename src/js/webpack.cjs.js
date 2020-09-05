94742f6ccb56ffac0bcfc84743435c1920799bd3
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2020-09-05 15:49:35 +0800

    CommomJS

--- src/js/webpack.cjs.js
+++ src/js/webpack.cjs.js
@@ -8,8 +8,17 @@
 // require: 讀入並執行 JavaScript，然後返回 module.exports。
 const utils = require('./webpack.cjs.utils')
 
-// module 可多次 require，但只在第一次加載運行，然後運行結果就被緩存，
-// 再次 require，就直接讀取緩存結果。要讓 module 再次加載運行，必須清除緩存。
+
+console.log('module path:', require.resolve('./webpack.cjs.utils')) // /Users/cicadawang/Documents/GitHub/study/src/js/webpack.cjs.utils.js
+console.log('entry point of the current application:', require.main.filename) // /Users/cicadawang/Documents/GitHub/study/src/js/webpack.cjs.js
+// 直接執行( node module.js ): require.main 指向 module 本身。
+console.log('in webpack.cjs:', 'require.main is module', require.main === module) // true
+// module.parent is null
+console.log('in webpack.cjs:', 'require.main is module.parent', require.main === module.parent) // false
+
+
+// module 可多次 require，但只在第一次加載執行，然後執行結果就被緩存，
+// 再次 require，就直接讀取緩存結果。要讓 module 再次加載執行，必須清除緩存。
 require('./webpack.cjs.utils').x = '10'
 console.log('x:', require('./webpack.cjs.utils').x) // x: 10
 // 清除所有 module 緩存
@@ -18,12 +27,6 @@ Object.keys(require.cache).forEach(function (key) {
 })
 // 清除 module 緩存後重新加載
 console.log('x:', require('./webpack.cjs.utils').x) // x: 5
-
-
-// 直接執行( node module.js ): require.main 指向 module 本身。
-console.log('in webpack.cjs:', 'require.main is module', require.main === module) // true
-// module.parent is null
-console.log('in webpack.cjs:', 'require.main is module.parent', require.main === module.parent) // false
 
 
 // require 後 counter 已被緩存, module 內 counter++ 的變化無法取得
--- src/js/webpack.cjs.utils.js
+++ src/js/webpack.cjs.utils.js
@@ -1,6 +1,6 @@
 // CommomJS module:
 
-// 調用執行( require('module') ): 若該 module 透過 require 執行，require.main 指向 parent。
+// 調用執行( require('module') ): 若該 module 透過 require 執行，require.main 指向 module.parent。
 console.log('in webpack.cjs.utils:', 'require.main is module', require.main === module) // false
 console.log('in webpack.cjs.utils:', 'require.main is module.parent', require.main === module.parent) // true
 
