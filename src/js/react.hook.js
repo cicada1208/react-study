32c2cfe5e5d5b98b725e40fe30f2dc2cd3dcf7d6
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2020-10-06 00:12:20 +0800

    Hook

--- src/js/react.hook.js
+++ src/js/react.hook.js
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from 'react'
 // Hook: 重複使用 stateful 邏輯
 export function HookEx() {
     // State Hook: 使 function component 能使用 React state。
-    // useState(): 傳入 state 起始值，回傳目前的 state 與更新 state 的 function，
+    // useState(): 傳入 state 起始值(可為 function 複雜邏輯計算後回傳起始值)，
+    // 回傳目前的 state 與更新 state 的 function，
     // 首次 render 予 state 起始值，其後 render 回傳最後更新的 state。
     // 可使用多個 useState() 宣告多個 state 變數。
     // count: 宣告的一個 state 變數，可為 number、string、object 等。
-    // setCount: 不像 class this.setState 會合併原本的 state，hook 更新 state 變數會直接取代。
+    // setCount: 不像 class this.setState 會合併原本的 state object，
+    // hook 更新 state 變數會直接取代，除非利用 setState(prevState => {return {...prevState, ...updatedValues}})。
+    // 如果用同樣的 state 值更新，則會跳過 render 及 effect 的執行。
     const [count, setCount] = useState(0)
 
     // side effect: fetch 資料、訂閱、手動改變 DOM。這些影響其他 component 且在 render 期間無法完成。
@@ -19,7 +22,7 @@ export function HookEx() {
     // 傳遞空 array([])，告訴 React 你的 effect 不依賴 props 或 state，因此不需重新執行，僅在 mount 和 unmount 時執行一次。
     // 可使用多個 useEffect() 故可區分相關邏輯在同一 effect function，並照指定順序執行。
     useEffect(() => {
-        // 需清除的 Effect: 會回傳 function，React 將在需要清除時執行它，預設在下次執行 effect 前清除前個 render 的 effect。
+        // 需清除的 Effect: 會回傳清除用的 function，React 將在需要清除時執行，預設在下次執行 effect 前清除前個 render 的 effect。
         // 此為無需清除的 Effect: 使用瀏覽器 API 更新標題。
         document.title = `You clicked ${count} times`
     }, [count])
@@ -30,8 +33,7 @@ export function HookEx() {
             {/* <button onClick={() => setCount(count + 1)}>+</button>
             <button onClick={() => setCount(count - 1)}>-</button> */}
 
-            {/* 傳遞一個 function 到 setCount，接收先前的 state，並回傳一個已更新的值。(基於先前的值來更新)
-             */}
+            {/* 傳遞一個 function 到 setCount，接收先前的 state，並回傳更新值(基於先前的值來更新)。 */}
             <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
             <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
         </>
