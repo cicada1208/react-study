eff2fc2238d0d1f231082fa1185102caff1d4601
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2020-10-10 21:50:17 +0800

    Equality

--- src/js/equality.js
+++ src/js/equality.js
@@ -1,6 +1,7 @@
-// ==: 一般相等("雙等於")，比較值
-// ===: 嚴格相等("三等於"、"全等")，比較值及型別
+// ==: 一般相等("雙等於")，不同型別先轉型後再比較值
+// ===: 嚴格相等("三等於"、"全等")，比較型別及值
 // Object.is: 和嚴格相等做同樣的事，但會將 NaN、-0、+0 獨立處理
+
 
 // objExp = false、0、''、null、undefined 判斷式印出 print
 let objExp = 0
@@ -41,29 +42,27 @@ console.log(`Object.is(0, ''):`, Object.is(0, '')) // false
 console.log('+0 == -0:', +0 == -0) // true
 console.log('+0 === -0:', +0 === -0) // true
 console.log('Object.is(+0, -0):', Object.is(+0, -0)) // false
+console.log('1 / +0 === 1 / -0:', 1 / +0 === 1 / -0) // false  Infinity !== -Infinity
+console.log('0 / 0:', 0 / 0) // NaN
 
 console.log('NaN == NaN:', NaN == NaN) // false
 console.log('NaN === NaN:', NaN === NaN) // false
 console.log('Object.is(NaN, NaN):', Object.is(NaN, NaN)) // true
 
-// object reference value is different (記憶體位址)
+// a new object reference value is different (記憶體位址)
 console.log(`{ x: 'test' } == { x: 'test' }:`, { x: 'test' } == { x: 'test' }) // false
 console.log(`{ x: 'test' } === { x: 'test' }:`, { x: 'test' } === { x: 'test' }) // false
 console.log(`Object.is({ x: 'test' }, { x: 'test' }):`, Object.is({ x: 'test' }, { x: 'test' })) // false
 
-
-// let ary = [1, 2]
-// let ary1 = ary
-// console.log(Object.is([1, 2], [1, 2]))
-// // let ary2 = ary.concat([])
-// // let ary3 = [...ary,]
-// let ary4 = [3, 4]
-// // ary.push(3, 4)
-// ary = [...ary4, ...ary]
-// console.log(ary1)
-// console.log(ary)
-// console.log(Object.is(ary, ary1))
-// // console.log(Object.is(ary, ary1))
-// // console.log(Object.is(ary, ary2))
-// // console.log(Object.is(ary, ary3))
-// // console.log(ary3)
+let ary = [1]
+let ary2 = ary
+console.log(`Object.is(ary, ary2):`, Object.is(ary, ary2)) // true
+let ary3 = ary.concat([])
+console.log(`ary === ary3:`, ary === ary3) // false
+console.log(`Object.is(ary, ary3):`, Object.is(ary, ary3)) // false
+let ary4 = [...ary,]
+console.log(`ary === ary4:`, ary === ary4) // false
+console.log(`Object.is(ary, ary4):`, Object.is(ary, ary4)) // false
+let ary5 = ary
+ary5.push(2)
+console.log(`Object.is(ary, ary5):`, Object.is(ary, ary5)) // true
