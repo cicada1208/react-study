882d29e3cfcf6da39533e36dfe1b8c7bc0bf92c1
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-04-04 01:44:16 +0800

    Object.assign()

---
+++ src/js/object.assign.js
@@ -0,0 +1,45 @@
+// The Object.assign() method copies all enumerable own properties
+// from one or more source objects to a target object.
+// It returns the target object.
+// Syntax: Object.assign(target, ...sources)
+
+const target = { a: 1, b: 2 };
+const source = { b: 3, c: 4 };
+const source2 = { d: 5, e: 6 };
+const returnedTarget = Object.assign(target, source, source2);
+console.log(target); // { a: 1, b: 3, c: 4, d: 5, e: 6 }
+console.log(returnedTarget); // { a: 1, b: 3, c: 4, d: 5, e: 6 }
+console.log(target === returnedTarget); // true
+
+// cloning an object
+const obj = { a: 1 };
+const copy = Object.assign({}, obj);
+console.log(copy); // { a: 1 }
+console.log(obj === copy); // false
+
+// For deep cloning, we need to use alternatives,
+// because Object.assign() copies property values.
+// If the source value is a reference to an object,
+// it only copies the reference value.
+let obj1 = { a: 0, b: { c: 0 } };
+let obj2 = Object.assign({}, obj1);
+console.log('obj2:', JSON.stringify(obj2)); // { "a": 0, "b": { "c": 0}}
+
+obj1.a = 1;
+console.log('obj1:', JSON.stringify(obj1)); // { "a": 1, "b": { "c": 0}}
+console.log('obj2:', JSON.stringify(obj2)); // { "a": 0, "b": { "c": 0}}
+
+obj2.a = 2;
+console.log('obj1:', JSON.stringify(obj1)); // { "a": 1, "b": { "c": 0}}
+console.log('obj2:', JSON.stringify(obj2)); // { "a": 2, "b": { "c": 0}}
+
+obj2.b.c = 3;
+console.log('obj1:', JSON.stringify(obj1)); // { "a": 1, "b": { "c": 3}}
+console.log('obj2:', JSON.stringify(obj2)); // { "a": 2, "b": { "c": 3}}
+
+// deep clone
+obj1 = { a: 0, b: { c: 0 } };
+let obj3 = JSON.parse(JSON.stringify(obj1));
+obj1.a = 4;
+obj1.b.c = 4;
+console.log('obj3:', JSON.stringify(obj3)); // { "a": 0, "b": { "c": 0}}
