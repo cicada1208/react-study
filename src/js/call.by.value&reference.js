d9be065438f334c1739ca1b275db0216af585efb
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-08-09 13:01:46 +0800

    call by reference

--- src/js/call.by.value&reference.js
+++ src/js/call.by.value&reference.js
@@ -1,4 +1,6 @@
 // call by value, call by reference ＆ call by sharing:
+// primitive types: string, number, boolean, null, undefined, bigint, symbol
+// object: primitive type 以外的，例如 array, function, map...
 // primitive types is call by value
 // object is call by reference or call by sharing(一種說法)
 // javascript 只有 call by value(與C++定義來比較)
@@ -34,3 +36,19 @@ function run(a) {
 var obj = { name: 'obj' };
 run(obj);
 console.log('obj:', obj); // obj: { name: 'obj' }
+
+console.log('call by reference:');
+var ref1 = [1];
+var ref2 = ref1;
+ref1.push(2);
+console.log('ref1:', ref1); // [1, 2]
+console.log('ref2:', ref2); // [1, 2]
+// == 和 ===，對於 object 比較的是址(reference)而非值(value)
+console.log('ref1 === ref2:', ref1 === ref2); // true
+var arr1 = ['Hi!'];
+var arr2 = ['Hi!'];
+console.log('arr1 === arr2:', arr1 === arr2); // false
+// 比較 object 內容是否相同，可轉換成 string 比較
+var arr1str = JSON.stringify(arr1);
+var arr2str = JSON.stringify(arr2);
+console.log('arr1str === arr2str:', arr1str === arr2str); // true
