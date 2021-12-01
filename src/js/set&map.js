286c99212b30b16f9c5cd9068306451ed247cca0
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-12-02 00:03:00 +0800

    WeakMap

--- src/js/set&map.js
+++ src/js/set&map.js
@@ -1,4 +1,4 @@
-// Set: 可按照插入的順序迭代元素，其中元素是唯一
+// Set: 可按插入的順序迭代元素，其中元素是唯一
 let set = new Set();
 set.add('hello').add('goodbye').add('hello');
 console.log('set.size:', set.size); // 2
@@ -7,15 +7,13 @@ console.log('set.has:', set.has('hello')); // true
 console.log('set iteration:');
 set.forEach(s => console.log(s));
 
-console.log('set to array:');
 let arySet = Array.from(set);
-console.log('arySet:', arySet); // [ 'hello', 'goodbye' ]
+console.log('set to array:', arySet); // [ 'hello', 'goodbye' ]
 let arySet2 = [...set];
-console.log('arySet2:', arySet2); // [ 'hello', 'goodbye' ]
+console.log('set to array:', arySet2); // [ 'hello', 'goodbye' ]
 
-console.log('array to set:');
 const nums = [2, 2, 3, 4, 4];
-console.log([...new Set(nums)]); // 去重 [ 2, 3, 4 ]
+console.log('array to set:', [...new Set(nums)]); // 去重 [ 2, 3, 4 ]
 
 let set2 = new Set();
 set2.add('hello').add('test');
@@ -24,7 +22,7 @@ console.log('intersection:', intersection); // Set { 'hello' }
 let difference = new Set([...set].filter(x => !set2.has(x)));
 console.log('difference:', difference); // Set { 'goodbye' }
 
-// Map: 可按照插入的順序迭代元素，鍵值唯一，鍵/值可為任何型別
+// Map: 可按插入的順序迭代元素，鍵值唯一，鍵或值可為任何型別
 let map = new Map();
 map.set('hello', 1);
 map.set('hello', 3);
@@ -43,13 +41,24 @@ map.forEach((val, key) => console.log('key:', key, ', val:', val));
 
 console.log('map.keys array:', [...map.keys()]);
 
+const obj = {
+  one: 1,
+  two: 2
+};
+let mapObj = new Map(Object.entries(obj));
+console.log('object to map:', mapObj);
+let objMap = Object.fromEntries(mapObj);
+console.log('map to object:', objMap);
+
 // WeakSet:
 let ws = new WeakSet();
 ws.add({ data: 4 });
 // Because the added object has no other references, it will not be held in the set
 
-// WeakMap:
+// WeakMap: 鍵只能為 Object，由於弱引用的特性及不可遍歷因此可應用在
+// 1. 對特定DOM節點添加狀態時，當DOM節點被刪，將DOM節點作為“鍵”的WeakMap也自動被回收
+// 2. 對類或構造函數中私有屬性綁定定義，當實例被刪，作為“鍵”的this消失，WeakMap自動回收
+// 3. 系統事件
 let wm = new WeakMap();
 wm.set(set, { extra: 3 });
 console.log('wm.get:', wm.get(set)); // { extra: 3 }
-console.log('wm.size:', wm.size); // undefined
