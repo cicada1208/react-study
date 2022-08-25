f6f4878d12b847c3f074fef40c0549387927e39f
cicada1208 <plumes@cycu.org.tw>
2022-08-26 00:10:59 +0800

    Regex.test

--- src/js/string.js
+++ src/js/string.js
@@ -1,7 +1,38 @@
 let str = 'a,b,c,d';
-console.log(str.split(',', 2)); //[ 'a', 'b' ]
-console.log(str.concat('1', '2')); // a,b,c,d12
+console.log('split:', str.split(',', 2)); // [ 'a', 'b' ]
+console.log('concat:', str.concat(',', 'z')); // a,b,c,d,z
 
-let str2 = 'a,b,1,2,c,d';
-console.log(str2.match(/\d/g)); // [ '1', '2' ]
-console.log(str2.search(/\d/)); // 4
+console.log('search:');
+let str2 = 'ab98cd';
+// search: if find return index else -1
+console.log('index:', str2.search(/\d/)); // index: 2
+
+console.log('match:');
+// not use global search: 回傳第一個匹配的字串
+console.log(str2.match(/\d/)); // [ '9', index: 2, input: 'ab98cd', groups: undefined ]
+// use global search: 回傳所有匹配的字串
+console.log(str2.match(/\d/g)); // [ '9', '8' ]
+
+console.log('Regex.test not use global search:');
+// Regex.test、Regex.exec not use global search:
+let str3 = 'abcdef';
+const reg3 = /c/;
+console.log(reg3.test(str3)); // true
+console.log(reg3.lastIndex); // 0
+console.log(reg3.test(str3)); // true
+console.log(reg3.lastIndex); // 0
+
+console.log('Regex.test use global search:');
+// Regex.test、Regex.exec use global search:
+// 會從 lastIndex 開始匹配，所以 test、exec 不使用 global search 較好
+let str4 = 'abcdef';
+let str4b = 'cabdef';
+const reg4 = /c/g;
+console.log(reg4.test(str4)); // true
+console.log(reg4.lastIndex); // 3, lastIndex 移至匹配字串下一索引
+console.log(reg4.test(str4)); // false
+console.log(reg4.lastIndex); // 0, 都找不到時 lastIndex 移至 0
+console.log(reg4.test(str4)); // true
+console.log(reg4.lastIndex); // 3
+console.log(reg4.test(str4b)); // false, 換另一字串仍是從 lastIndex 開始匹配
+console.log(reg4.lastIndex); // 0
