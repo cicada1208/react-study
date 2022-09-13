a5edb403de618d9f43378645795863dfee67cb30
cicada1208 <plumes@cycu.org.tw>
2022-09-13 23:50:49 +0800

    dayjs

--- src/js/dayjs.js
+++ src/js/dayjs.js
@@ -1,10 +1,10 @@
 const dayjs = require('dayjs');
 require('dayjs/locale/zh-tw'); // 导入本地化语言
 const customParseFormat = require('dayjs/plugin/customParseFormat'); // 导入插件
-const isLeapYear = require('dayjs/plugin/isLeapYear');
+const minMax = require('dayjs/plugin/minMax');
 
 dayjs.extend(customParseFormat); // 使用插件
-dayjs.extend(isLeapYear);
+dayjs.extend(minMax);
 dayjs.locale('zh-tw'); // 使用本地化语言
 
 console.log(dayjs().format('YYYY/MM/DD HH:mm:ss')); // now
@@ -32,3 +32,21 @@ console.log(
   ).format('YYYY-MM-DD HH:mm:ss')
 ); // strict: 2012-05-28 10:21:15
 // 嚴格校驗須提供 format & strict
+
+console.log('year:', dayjs().get('year'));
+console.log('month:', dayjs().get('month'));
+console.log('date:', dayjs().get('date'));
+
+// 需使用插件 minMax
+console.log(
+  'max:',
+  dayjs
+    .max(dayjs(), dayjs('2018-01-01'), dayjs('2019-01-01'))
+    .format('YYYY-MM-DD HH:mm:ss')
+);
+console.log(
+  'min:',
+  dayjs
+    .min([dayjs(), dayjs('2018-01-01'), dayjs('2019-01-01')])
+    .format('YYYY-MM-DD HH:mm:ss')
+);
