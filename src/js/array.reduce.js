acd5aa8bd8ac67ebc1f8c5a8426c0e5779f81cdb
cicada1208 <plumes@cycu.org.tw>
2022-09-21 23:41:18 +0800

    array.reduce

--- src/js/array.reduce.js
+++ src/js/array.reduce.js
@@ -1,6 +1,6 @@
 // 將陣列內元素全部加總
 var arr = [1, 2, 3];
-var sum = arr.reduce((accumulator, currentValue) => {
-  return (accumulator += currentValue);
-});
+var sum = arr.reduce(
+  (previousValue, currentValue) => previousValue + currentValue
+);
 console.log(sum); // 6
