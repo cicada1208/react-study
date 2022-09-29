174a4b372e6c9bed47177c0c85ab03971c17835b
cicada1208 <plumes@cycu.org.tw>
2022-09-29 17:30:47 +0800

    differenceBy

--- src/js/lodash.js
+++ src/js/lodash.js
@@ -2,4 +2,21 @@ const _ = require('lodash');
 
 let obj1 = { a: 'a' };
 let obj2 = obj1;
-console.log(_.difference([obj1, 1], [2, obj2])); // [ 1 ]
+console.log('difference:', _.difference([obj1, 1], [2, obj2])); // [ 1 ]
+
+console.log(
+  'differenceBy:',
+  _.differenceBy([2.9, 1.2], [2.3, 3.4], Math.floor)
+); // [ 1.2 ]
+
+console.log(
+  'differenceBy:',
+  _.differenceBy(
+    [
+      { x: 2, y: 0 },
+      { x: 1, y: 1 }
+    ],
+    [{ x: 1, y: 2 }],
+    'x'
+  )
+); // [ { x: 2 } ]
