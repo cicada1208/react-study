2874f1a39156e7f0e90dd0bf43a090ff944dde2f
cicada1208 <plumes@cycu.org.tw>
2022-10-05 23:57:23 +0800

    lodash

--- src/js/lodash.js
+++ src/js/lodash.js
@@ -64,3 +64,41 @@ console.log('countBy:', _.countBy(['three', 'one', 'two'], 'length')); // { '3':
 console.log('groupBy:', _.groupBy([6.1, 4.2, 6.3], Math.floor)); // { '4': [4.2], '6': [6.1, 6.3] }
 
 console.log('groupBy:', _.groupBy(['one', 'two', 'three'], 'length')); // { '3': ['one', 'two'], '5': ['three'] }
+
+console.log(
+  'groupBy:',
+  _.groupBy(
+    [
+      { user: 'barney', group: 1 },
+      { user: 'fred', group: 2 },
+      { user: 'amy', group: 1 }
+    ],
+    'group'
+  )
+);
+// {
+//   '1': [ { user: 'barney', group: 1 }, { user: 'amy', group: 1 } ],
+//   '2': [ { user: 'fred', group: 2 } ]
+// }
+
+console.log('includes:', _.includes({ a: 1, b: 2 }, 1));
+
+console.log(
+  'invokeMap:',
+  _.invokeMap(
+    [
+      [5, 1, 7],
+      [3, 2, 1]
+    ],
+    'sort'
+  )
+); // [ [ 1, 5, 7 ], [ 1, 2, 3 ] ]
+
+console.log('invokeMap:', _.invokeMap([123, 456], String.prototype.split, '')); // [ [ '1', '2', '3' ], [ '4', '5', '6' ] ]
+
+console.log(
+  'map:',
+  _.map({ a: 4, b: 8 }, n => n * n)
+); // [16, 64] (iteration order is not guaranteed)
+
+console.log('map:', _.map([{ user: 'barney' }, { user: 'fred' }], 'user')); // ['barney', 'fred']
